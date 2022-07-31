b60504fa5acccaa7c72442465fb7284ff58b9b41
shaoguixin <2404987607@qq.com>
2022-07-31 10:25:46 +0800

    用cpp11 thread 封装线程类

--- Base/Thread.cc
+++ Base/Thread.cc
@@ -3,42 +3,53 @@
 
 using namespace bing;
 
+//静态成员变量在类外初始化
 static std::atomic_int32_t numCreated_(0);
 
 Thread::Thread(ThreadFunc func, const std::string& name) 
-    : running_(false), joined_(false), pid_(0), tid_(0), func_(std::move(func)), latch_(1), name_(name)
+    : running_(false), joined_(false), tid_(0), func_(std::move(func)), latch_(1), name_(name)
     {
         setDefaultName();       //设施默认的名字
     }
 
 Thread::~Thread() {
-    //线程运行了才进行西沟
+    //线程运行了才进行析构
     //joined_是主线程需要等待子线程结束
     //如果不是的话就是守护线程，detach 
     //没有等待子线程结束，直接detach自生自灭就好了
     if (running_ && !joined_) {
-        pthread_detach(pid_);       //直接分离子线程就行了
+        thread_->detach();          //不用等待子线程结束的话， 直接分离就好了
     }
 }
 
 //等待子线程退出
 void Thread::join() {
+    assert(running_);
+    assert(!joined_);
     joined_ = true;
-    pthread_join(pid_, NULL);
+    thread_->join();
 }
 
-
 void Thread::start() {
-    assert(!running_);
     running_ = true;
 
-    if (pthread_create(&pid_, NULL, &func_, NULL)) {
-        //创建失败了 
-    } else {
-        latch_.wait();      //等待计数为0
-        assert(tid_ > 0);
-    }
+    /*
+        创建线程， thread_指针指向这个线程的对象，
+        使用lambda捕获线程对象，访问线程的成员变量
+    */
+   thread_ = std::shared_ptr<std::thread>(new std::thread([&]() {
+        //获得线程的标识符号
+        tid_ = currentThread::tid();
 
+        //线程类创建的时候就设定了latch(1)
+        latch_.countDown();
+        
+        //运行函数
+        func_();
+   }
+    ));
+
+    latch_.wait();
 
 }
 
--- Base/Thread.h
+++ Base/Thread.h
@@ -6,7 +6,8 @@
 
 
 #include <functional>
-#include <pthread.h>
+// #include <pthread.h>    //use cpp11 thread 
+#include <thread>
 #include <atomic>
 #include <unistd.h>
 #include <string>
@@ -16,6 +17,8 @@ namespace bing {
 class Thread : nocopyable{ 
  public: 
     using ThreadFunc = std::function<void()>;
+
+   //  typedef std::function<void ()> ThreadFunc;
 
     explicit Thread(ThreadFunc func, const std::string& name = std::string());
 
@@ -31,13 +34,20 @@ class Thread : nocopyable{
 
  private:
     void setDefaultName();
-    
-    pid_t tid_;              //线程的标识
-    pthread_t pid_;           //进行线程的操作的句柄
-    std::string name_;       //线程的名称
-    ThreadFunc func_;        //线程的执行函数
-    bool running_;         
-    bool joined_;             //主线程等待子线程
+    bool running_;                     //开始线程
+    bool joined_;                      //主线程等待子线程运行完毕
+
+    pid_t tid_;                        //线程的标识
+   //  pthread_t pid_;                 //进行线程的操作的句柄
+
+   /*
+      如果直接调用std::thread thread_, 
+      就直接启动线程了，用智能指针进行封装
+   */
+    std::shared_ptr<std::thread> thread_;       //创建线程，用智能指针包装
+    std::string name_;                 //线程的名称
+    ThreadFunc func_;                  //线程的执行函数
+
     CountDownLatch latch_;
 
     static std::atomic_int32_t numCreated_; // 记录产生线程的个数(静态变量)
